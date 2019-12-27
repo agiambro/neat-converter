ac0f90b2cdbba36b05b13242a5801ef4b5655d59
agiambro <giambrone@accuratebiz.com>
2019-12-27 15:13:51 -0600

    wip parse Document title without spaces; parse date

--- app/lib/renamers/document.rb
+++ app/lib/renamers/document.rb
@@ -4,9 +4,27 @@ class Document
   end
 
   def rename_file
+    title = parse_title
+    date = parse_date
+    "#{[title, date].join('-')}.pdf"
   end
 
 
   private
 
+  def parse_title
+    regex = /Document-(.*)(Mon|Tue|Wed|Thu|Fri|Sat|Sun)/
+    matches = @file_name.match regex
+    matches[1].strip
+  end
+
+  def parse_date
+    Chronic.parse(extract_date).strftime('%Y-%m-%d')
+  end
+
+  def extract_date
+    regex = /(Mon|Tue|Wed|Thu|Fri|Sat|Sun)[^.$]*/
+    matches = @file_name.match regex
+    clean_date = matches[0].strip
+  end
 end
--- spec/app/lib/renamers/document_spec.rb
+++ spec/app/lib/renamers/document_spec.rb
@@ -10,19 +10,16 @@ RSpec.describe Document do
 
       context 'with spaces' do
         it 'renames' do
+          file_name = 'Document-111 W Street - Lead Based Paint Thu Mar 19 2009'
+          expect(Document.new(file_name).rename_file).to eq '111 W Street-Lead Based Paint-2009-03-19.pdf'
         end
       end
     end
 
     context 'with scanner name' do
       it 'renames' do
-
-      end
-    end
-
-    context 'without scanner name' do
-      it 'renames' do
-
+        file_name = 'Document-111 W Street - return of gifts Fri Apr 27 2012 ScanSnap Manager S1500M'
+        expect(Document.new(file_name).rename_file).to eq '111 W Street-return of gifts-2012-04-27.pdf'
       end
     end
   end
